eec5b45701dc3267d49a14a5beb08e8feea61a39
unknown <khizar062@gmail.com>
2020-08-10 23:08:14 -0700

    all games and single game getter

--- controllers/game.js
+++ controllers/game.js
@@ -1,5 +1,20 @@
 const Game = require("../models/game");
 
+
+exports.gameById = (req, res, next, id)=> {
+    Game.findById(id)
+        .exec((err, game)=>{
+            if(err|| !game){
+                return res.status(400).json({
+                    error: "game not found"
+                });
+            }
+            req.game = game;
+            next();
+        });
+};
+
+
 exports.createGame = async (req, res) => {
     req.body.name = req.body.name.toLowerCase();
 
@@ -14,4 +29,13 @@ exports.createGame = async (req, res) => {
         }
         return res.json(result)
     })
+}
+
+exports.getGames = (req, res) => {
+    Game.find((err, game)=> {
+        return res.json(game)
+    })
+}
+exports.getSingleGame = (req, res)=> {
+    return res.json (req.game);
 }
--- routes/game.js
+++ routes/game.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const {createGame} = require("../controllers/game");
+const {createGame, getGames, gameById, getSingleGame} = require("../controllers/game");
 
 router.post("/create_game" , createGame);
+router.get("/games" , getGames);
+router.get("/game/:gameId" , getSingleGame);
 
+router.param("gameId", gameById);
 module.exports = router;
