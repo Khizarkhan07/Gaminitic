b90ce95fd84f9c2bf22327ca0fe81ea120baa1e1
unknown <khizar062@gmail.com>
2020-08-28 02:57:40 -0700

    set preference

--- controllers/preference.js
+++ controllers/preference.js
@@ -3,10 +3,26 @@ const Console = require("../models/console");
 const Preference = require("../models/preference");
 const User = require("../models/user");
 
+exports.prefById = (req, res, next, id)=> {
+    Preference.findById(id)
+        .exec((err, pref)=>{
+            if(err|| !pref){
+                return res.status(400).json({
+                    errors: {
+                        preference: "preference not found"
+                    }
+                });
+            }
+            req.pref = pref;
+            next();
+        });
+};
+
+
+
 exports.set_preference = (req, res) => {
-    const {consoleId, gameId, difficulty, length} = req.body
+    const {selectedGame, selectedConsole, difficulty, length} = req.body
     console.log(req.auth._id)
-    console.log(consoleId)
     User.findOne({_id: req.auth._id}, (err, user)=> {
         if(err||!user){
             return res.status(403).json ({
@@ -16,7 +32,7 @@ exports.set_preference = (req, res) => {
             })
         }
         else {
-            Console.findById(consoleId, (err, console)=> {
+            Console.findById(selectedConsole, (err, console)=> {
                 if(err||!console){
                     return res.status(400).json ({
                         errors: {
@@ -25,7 +41,7 @@ exports.set_preference = (req, res) => {
                     })
                 }
                 else {
-                    Game.findById(gameId, (err, game)=> {
+                    Game.findById(selectedGame, (err, game)=> {
                         if(err||!game){
                             return res.status(400).json ({
                                 errors: {
@@ -37,9 +53,9 @@ exports.set_preference = (req, res) => {
                             let pref = new Preference();
                             pref.difficulty= req.body.difficulty;
                             pref.length= req.body.length;
-                            pref.user_id = user;
-                            pref.console_id= console;
-                            pref.game_id = game
+                            pref.userId = user;
+                            pref.selectedConsole= console;
+                            pref.selectedGame = game
 
                             pref.save((err, pref)=> {
                                 if(err||!pref){
@@ -85,4 +101,14 @@ exports.allPrefrences = (req, res)=> {
             }).populate('selectedConsole', 'name photo').populate('selectedGame', 'name photo')
         }
     })
+}
+
+exports.deletePreference = (req, res)=> {
+    if(req.auth._id != req.pref.userId){
+        return res.status(403).json({
+            errors: {
+                auth: "You are unauthorized to perform this action"
+            }
+        })
+    }
 }
--- routes/prefernce.js
+++ routes/prefernce.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const {set_preference, allPrefrences} = require("../controllers/preference");
+const {set_preference, allPrefrences, deletePreference, prefById} = require("../controllers/preference");
 const {gameById} = require("../controllers/game");
 const {userById} = require("../controllers/user");
 const {requireSignin} = require('../controllers/auth')
 
 router.post("/set_preference" , requireSignin, set_preference)
 router.get("/get_preferences" , requireSignin, allPrefrences)
+router.delete("/delete_preference/:prefId" , requireSignin, deletePreference)
 
 router.param("gameId", gameById);
 router.param("userId", userById);
+router.param("prefId", prefById);
 module.exports = router;
